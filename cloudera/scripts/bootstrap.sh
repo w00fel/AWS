f9f6c36125beced63c7868c731004287d54bec03
Steve Biedlingmaier <steve@woofel.net>
2015-09-30 21:13:26 -0700

    Added bootsrap/terminate scripts for Cloudera Director.

---
+++ cloudera/scripts/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Install Cloudera CDH.
+#
+VERSION=1.5.0
+
+USERNAME=ec2-user
+USERHOME=/home/${USERNAME}
+CLOUDERA=${USERHOME}/cloudera
+
+DIRECTORDIR=${CLOUDERA}/cloudera-director-client-${VERSION}
+AWS_CLUSTER_CONF=${DIRECTORDIR}/aws.cluster.conf
+
+${DIRECTORDIR}/bin/cloudera-director bootstrap ${AWS_CLUSTER_CONF}
